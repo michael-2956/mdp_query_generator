3379ba606d73ac80a45e4d674d59f7cfa40049bc
Oleksandr Marmaliuk <95655470+alexmarmaliuk@users.noreply.github.com>
2022-06-28 01:06:59 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,12 @@ fn main() {
             subquery: Box::new(create_select(
                 vec![SelectItem::UnnamedExpr(create_compound_identifier("S.A"))],
                 vec![create_table("S")],
-                None
+                None,
+                false
             )),
             negated: true,
-        })
+        }),
+        false
     );
 
     println!("Query 1: {:#?}", q1.to_string());
@@ -30,11 +32,38 @@ fn main() {
                     left: Box::new(create_compound_identifier("S.A")),
                     op: BinaryOperator::Eq,
                     right: Box::new(create_compound_identifier("R.A"))
-                })
+                }),
+                false
             )),
             negated: true
-        })
+        }),
+        false
     );
 
     println!("Query 2: {:#?}", q2.to_string());
+
+    //SELECT DISTINT X.A FROM R X, R Y WHERE X.A=Y.A
+    let q3 = create_select(
+        vec![SelectItem::UnnamedExpr(create_compound_identifier("R.A"))],
+        vec![create_table("R X, R Y")],
+        Some(Expr::BinaryOp {
+            left: Box::new(create_compound_identifier("X.A")),
+            op: BinaryOperator::Eq,
+            right: Box::new(create_compound_identifier("Y.A"))
+        }),
+        true
+    );
+
+    println!("Query 3: {:#?}", q3.to_string());
+
+    //SELECT DISTINT R.A FROM R
+    let q4 = create_select(
+        vec![SelectItem::UnnamedExpr(create_compound_identifier("R.A"))],
+        vec![create_table("R")],
+        None,
+        true
+    );
+
+    println!("Query 4: {:#?}", q4.to_string());
+
 }
