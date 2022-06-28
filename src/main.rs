1653b4f46981066c5ff2ab94ddad61dbd55fb29d
michael-2956 <work.michael.2956@gmail.com>
2022-06-28 19:43:08 +0300

    add exmaples folder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlparser::{ast::{SelectItem, Expr, BinaryOperator}};
+use sqlparser::{ast::{SelectItem, Expr}};
 
 use equivalence_testing::query_creation::helpers::{create_select, create_compound_identifier, create_table};
 
@@ -19,48 +19,5 @@ fn main() {
         }),
         false
     );
-    println!("Query 1: {:#?}", q1.to_string());
-
-    // SELECT R.A FROM R WHERE NOT EXISTS (SELECT S.A FROM S WHERE S.A = R.A)
-    let q2 = create_select(
-        vec![SelectItem::UnnamedExpr(create_compound_identifier("R.A"))],
-        vec![create_table("R", None)],
-        Some(Expr::Exists {
-            subquery: Box::new(create_select(
-                vec![SelectItem::UnnamedExpr(create_compound_identifier("S.A"))],
-                vec![create_table("S", None)],
-                Some(Expr::BinaryOp {
-                    left: Box::new(create_compound_identifier("S.A")),
-                    op: BinaryOperator::Eq,
-                    right: Box::new(create_compound_identifier("R.A"))
-                }),
-                false
-            )),
-            negated: true
-        }),
-        false
-    );
-    println!("Query 2: {:#?}", q2.to_string());
-
-    // SELECT DISTINT X.A FROM R X, R Y WHERE X.A=Y.A
-    let q3 = create_select(
-        vec![SelectItem::UnnamedExpr(create_compound_identifier("X.A"))],
-        vec![create_table("R", Some("X")), create_table("R", Some("Y"))],
-        Some(Expr::BinaryOp {
-            left: Box::new(create_compound_identifier("X.A")),
-            op: BinaryOperator::Eq,
-            right: Box::new(create_compound_identifier("Y.A"))
-        }),
-        true
-    );
-    println!("Query 3: {:#?}", q3.to_string());
-
-    // SELECT DISTINT R.A FROM R
-    let q4 = create_select(
-        vec![SelectItem::UnnamedExpr(create_compound_identifier("R.A"))],
-        vec![create_table("R", None)],
-        None,
-        true
-    );
-    println!("Query 4: {:#?}", q4.to_string());
+    println!("Query: {:#?}", q1.to_string());
 }
