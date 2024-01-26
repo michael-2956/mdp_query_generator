52629c9dd1f8ce5c7323e6b87146e6c1416d4ace
michael-2956 <work.michael.2956@gmail.com>
2024-01-26 12:11:27 +0200

    finalize performance graph creation cocde

--- create_perf_version.py
+++ create_perf_version.py
@@ -4,11 +4,11 @@ FROM_multiple_relations -> call0_Query
 
 limit_num [label="limit [num]\\nmod: 'single row' -> OFF", modifier="single row", modifier_mode="off"]
 LIMIT -> limit_num
-call52_types [TYPES="[integer, numeric, bigint]", MODS="[no column spec, no aggregate]", label="TYPES: [integer, numeric, bigint]\nMODS: [no column spec, no aggregate]", shape=rectangle, style=filled, color=lightblue]
+call52_types [TYPES="[integer, numeric, bigint]", MODS="[no column spec, no aggregate]", label="TYPES: [integer, numeric, bigint]\\nMODS: [no column spec, no aggregate]", shape=rectangle, style=filled, color=lightblue]
 limit_num -> call52_types
 call52_types -> EXIT_LIMIT
 
-types_return_typed_null [label="Typed null\nmod.: 'no typed nulls' -> OFF", modifier="no typed nulls", modifier_mode="off"]
+types_return_typed_null [label="Typed null\\nmod.: 'no typed nulls' -> OFF", modifier="no typed nulls", modifier_mode="off"]
 types_select_type_bigint -> types_return_typed_null
 types_select_type_integer -> types_return_typed_null
 types_select_type_numeric -> types_return_typed_null
@@ -27,7 +27,7 @@ graph = open("graph.dot", 'r').read()
 for line in to_comment.split('\n'):
     if line != '':
         new_line = "// " + line
-        graph.replace(line, new_line)
+        graph = graph.replace(line, new_line)
 
 open("performance_untrained_graph.dot", 'w').write(graph)
 
