cb00f2d0723cf49b3b77816f30115f8046437c75
michael-2956 <work.michael.2956@gmail.com>
2024-10-13 16:22:08 +0300

    initial set expressions

--- src/query_creation/query_generator/ast_builders/set_expression.rs
+++ src/query_creation/query_generator/ast_builders/set_expression.rs
@@ -1,6 +1,6 @@
-use sqlparser::ast::SetExpr;
+use sqlparser::ast::{SetExpr, SetOperator, SetQuantifier};
 
-use crate::{query_creation::{query_generator::{match_next_state, QueryGenerator}, state_generator::state_choosers::StateChooser}, unwrap_variant};
+use crate::{query_creation::{query_generator::{ast_builders::query::QueryBuilder, match_next_state, QueryGenerator}, state_generator::{markov_chain_generator::markov_chain::QueryTypes, state_choosers::StateChooser, CallTypes}}, unwrap_pat, unwrap_variant};
 
 use super::select_query::SelectQueryBuilder;
 
@@ -32,7 +32,45 @@ impl SetExpressionBuilder {
             "call0_SELECT_query" => {
                 let select_body = &mut **unwrap_variant!(body, SetExpr::Select);
                 SelectQueryBuilder::build(generator, select_body);
-            }
+            },
+            "call7_Query" => {
+                *body = SetExpr::Query(Box::new(QueryBuilder::nothing()));
+                QueryBuilder::build(generator, &mut **unwrap_variant!(body, SetExpr::Query));
+            },
+            "set_expression_set_operation" => {
+                let arg = unwrap_variant!(generator.state_generator.get_fn_selected_types_unwrapped(), CallTypes::QueryTypes);
+                assert!(matches!(arg, QueryTypes::ColumnTypeLists { .. }));  // needed for us to work properly
+                // *body = SetExpr::;
+                let op = match_next_state!(generator, {
+                    "query_set_op_intersect" => SetOperator::Intersect,
+                    "query_set_op_union" => SetOperator::Union,
+                    "query_set_op_except" => SetOperator::Except,
+                });
+                *body = SetExpr::SetOperation {
+                    op,
+                    set_quantifier: SetQuantifier::None,
+                    left: Box::new(Self::nothing()),
+                    right: Box::new(Self::nothing()),
+                };
+                let left = &mut **unwrap_pat!(body, SetExpr::SetOperation { left, .. }, left);
+                match_next_state!(generator, {
+                    "call3_set_expression" => Self::build(generator, left),
+                    "call2_set_expression" => Self::build(generator, left),
+                    "call5_Query" => {
+                        *left = SetExpr::Query(Box::new(QueryBuilder::nothing()));
+                        QueryBuilder::build(generator, &mut **unwrap_variant!(left, SetExpr::Query));
+                    }
+                });
+                let right = &mut **unwrap_pat!(body, SetExpr::SetOperation { right, .. }, right);
+                match_next_state!(generator, {
+                    "call4_set_expression" => Self::build(generator, right),
+                    "call5_set_expression" => Self::build(generator, right),
+                    "call6_Query" => {
+                        *right = SetExpr::Query(Box::new(QueryBuilder::nothing()));
+                        QueryBuilder::build(generator, &mut **unwrap_variant!(right, SetExpr::Query));
+                    }
+                });
+            },
         });
 
         generator.expect_state("EXIT_set_expression");
